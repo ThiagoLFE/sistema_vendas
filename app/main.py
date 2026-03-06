482f3409421c75a3ab6c59f77820a90c7e17b1d2
thiagofirered <thiago.rib1905@gmail.com>
2026-03-05 23:10:26 -0300

    feat: created a sqlite db with sqlalchemy library

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from app.database.connection import Base, engine, SessionLocal
+from app.models.product import Product
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
     return {"message": "API funcionando"}
+
+@app.post("/create-product")
+def create_product():
+
+    db = SessionLocal()
+
+    new_product = Product(
+        name="Mouse",
+        price=150.0,
+        quantity=10
+    )
+
+    db.add(new_product)
+    db.commit()
+    db.close()
+    
+    return {"message": "Produto criado"}
